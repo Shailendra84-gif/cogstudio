f2288af13c4aa9934db5b81fdd29eaa77e4ff13f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-18 23:02:41 -0500

    update

--- install.js
+++ install.js
@@ -50,11 +50,11 @@ module.exports = {
         }
       }
     },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
-      }
-    }
+//    {
+//      method: "fs.link",
+//      params: {
+//        venv: "app/env"
+//      }
+//    }
   ]
 }
